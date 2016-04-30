038857858dbfa8c26c8cb0f580ce2aa63168e220
crasneanucristian <crasneanu.cristian@gmail.com>
2016-04-30 20:58:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://heroku_h6zg9h0d:2o2djeebl7gu50vvqi0ilhab4b@ds011462.mlab.com:11462/heroku_h6zg9h0d',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || 'emailTestAppId',
+  masterKey: process.env.MASTER_KEY || 'emailTestMasterKey', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
