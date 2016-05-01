5962f0abe67eb4dbbdc69c643a3a2295f8691a17
crasneanucristian <crasneanu.cristian@gmail.com>
2016-05-01 18:52:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
+var SimpleMailgunAdapter = require('parse-server/lib/Adapters/Email/SimpleMailgunAdapter');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
